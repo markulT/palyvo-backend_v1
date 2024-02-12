349dafff51099ad9b8be9dc5790b260974f3dbd6
Marco Bobr <marrcokyr@gmail.com>
2024-02-12 23:12:08 +0200

    aboba

--- tools/data/roles.go
+++ tools/data/roles.go
@@ -2,6 +2,9 @@ package data
 
 import (
 	"context"
+	"github.com/google/uuid"
+	"go.mongodb.org/mongo-driver/bson"
+	"golang.org/x/crypto/bcrypt"
 	"palyvoua/internal/models"
 	"palyvoua/tools"
 )
@@ -38,5 +41,60 @@ func (s *defaultDBSeeder) createDefaultRoles() error {
 
 
 
+	return nil
+}
+
+func (s *defaultDBSeeder) createUser(c context.Context,email string, password string, roleName string) (*models.User, error) {
+
+	roleCollection :=tools.DB.Collection("roles")
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+
+	if err!=nil {
+		return nil,err
+	}
+
+	userID, err := uuid.NewRandom()
+	if err!=nil {
+		return nil,err
+	}
+
+	var adminRole models.Role
+
+	res := roleCollection.FindOne(c, bson.M{"name":roleName})
+	if res.Err()!= nil {
+		return nil,res.Err()
+	}
+
+	if err = res.Decode(&adminRole);err!=nil {
+		return nil,err
+	}
+
+	admin := models.User{
+		Email:      email,
+		Password:   string(hashedPassword),
+		ID:         userID,
+		CustomerID: "",
+		Role:       adminRole.ID,
+	}
+
+	return &admin, nil
+}
+
+func (s *defaultDBSeeder) createDefaultUsers() error {
+	ctx := context.Background()
+	var insert []interface{}
+	var err error
+	userCollection :=tools.DB.Collection("users")
+	admin, err := s.createUser(ctx, "admin", "1234", "ROLE_ADMIN")
+	if err != nil {
+		return err
+	}
+	insert = append(insert, admin)
+
+	_,err = userCollection.InsertMany(ctx, insert)
+	if err!=nil {
+		return err
+	}
 	return nil
 }
